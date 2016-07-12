18e5776faf457f816102d1b0b7c7543dfaffe53b
Dan Siegel and Marissa Siegel <dan@>
2016-07-11 20:41:50 -0700

    add click and hold buttons and additional functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,37 @@
 function randomnumber() {
   return Math.floor(Math.random() * (6)) + 1;
 }
-var play = function (currentNumbers) {
-  var turnNumbers = []
-  if (randomnumber>1){
-    turnNumbers.push(randomnumber)
-  }
-  else if (randomnumber === 1){
-    (turnNumbers = []);
+var turnNumbers = []
+var roll = function() {
+  var something = randomnumber()
+  if(something>1) {
+    turnNumbers.push(something);
+  } else if (something === 1) {
+    turnNumbers = []
   }
 }
-
-
-
 
 function Player(currentScore, turnNumbers) {
   this.currentScore = currentScore;
   this.turnNumbers = turnNumbers;
 }
 
+
+
 Player.prototype.display = function () {
-  return this.currentScore + " " this.turnNumbers;
+  return this.currentScore + " " + this.turnNumbers;
   }
 
-var Player1 = new Player();
-var Player2 = new Player();
+var Player1 = new Player("Player1", [], true);
+var Player2 = new Player("Player2", [], false);
 $(document).ready(function() {
+  $("form#dice").submit(function() {
+    event.preventDefault();
+    var numb = randomnumber();
+    roll() //add the array code, maybe map//
+
+    console.log(play())
+
+  });
 
 });
